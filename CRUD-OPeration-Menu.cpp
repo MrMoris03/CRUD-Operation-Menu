c8e8484c021ad28754e2073c31c4a4298b379a43
MrMoris03 <yusuphmoris03@gmail.com>
2025-01-21 12:58:10 +0300

    Structure of an Item
    Structure of an Item, with the function to display its details.

---
+++ CRUD-OPeration-Menu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+//Structure of an Item.
+struct Item {
+	int id;
+	string name;
+	double price;
+	
+	//Display Item details.
+	void display () const {
+	cout << "ID: " << id << ", Name: " << name << ", Price: $" << price << endl;
+	
+	}
+};
