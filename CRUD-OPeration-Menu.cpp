c186d1388c27e5eee83a5631ed5b76cf8c815ac7
MrMoris03 <yusuphmoris03@gmail.com>
2025-01-24 13:02:08 +0300

    comments
    Clear input buffer to avoid issues with getline

--- CRUD-OPeration-Menu.cpp
+++ CRUD-OPeration-Menu.cpp
@@ -66,5 +66,5 @@ void createItem(vector<Item>& items) {
 	
 	cout << "\nEnter Item ID: ";
 	cin >> newItem.id;	//Input item ID
-	cin.ignore();
+	cin.ignore();	//Clear input buffer to avoid issues with getline
 }
